8345947f859e4f2eeaac343294d25c2144a71d82
Deegoh <tiagopintomonteiro@gmail.com>
2021-10-09 23:03:25 +0200

    finish ft_memmove

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -2,7 +2,23 @@
 
 void	*ft_memmove(void *dst, void *src, size_t n)
 {
-	(void) src;
-	(void) n;
+	size_t	i;
+
+	i = -1;
+	if (dst == src)
+		return (0);
+	if (src > dst)
+	{
+		while (++i < n)
+			((char *)dst)[i] = ((char *)src)[i];
+	}
+	else
+	{
+		while(n)
+		{
+			((char *)dst)[n - 1] = ((char *)src)[n - 1];
+			n--;
+		}
+	}
 	return (dst);
 }
