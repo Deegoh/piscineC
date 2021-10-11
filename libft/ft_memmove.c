e381e7c8331cd842700d89ec126afa8dcc59d5d2
Deegoh <tiagopintomonteiro@gmail.com>
2021-10-11 19:40:53 +0200

    :sparkles: add header

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpinto-m <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/11 19:08:01 by tpinto-m          #+#    #+#             */
+/*   Updated: 2021/10/11 19:10:15 by tpinto-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 void	*ft_memmove(void *dst, void *src, size_t n)
@@ -14,7 +26,7 @@ void	*ft_memmove(void *dst, void *src, size_t n)
 	}
 	else
 	{
-		while(n)
+		while (n)
 		{
 			((char *)dst)[n - 1] = ((char *)src)[n - 1];
 			n--;
